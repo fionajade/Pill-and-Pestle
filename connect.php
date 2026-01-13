41d941bc22668d136aef15b99f06d76c274b3cb2
fionajade <jadee.fiona4@gmail.com>
2026-01-13 18:03:10 +0800

    updated the connect

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 $dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "";
-$db = "meditrack";
+$db = "pill-and-pestle";
 
 $conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n" . $conn->error);
 
